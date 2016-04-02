46a6b2a0a0b8b5c5c4ae7ca403110a6deb82cfd9
Gradecak <gradecam@tcd.ie>
2016-04-02 12:27:04 +0100

    added swipe gesture

--- js/content_script.js
+++ js/content_script.js
@@ -1,11 +1,20 @@
 // distance the user must scroll for it to be considered a swipe
 //  TODO change scroll distance to depend on screen resolution
-var scrollDistance = 40;
+var scrollDelta = 100;
+//drag gesture handler
+// if the user is at the top of the page
+if($(window).scrollTop() === 0){
+  var startPos
+  var endPos
 
-$(document).on('mousedown', function(e){
-  // if the user is at the top of the page
-  if($(window).scrollTop() == 0){
-    console.log("Page X:"+ e.pageX + "Page Y" + e.pageY)
-    console.log("Client X:"+ e.clientX + "Client Y" + e.clientY)
-  }
-})
+  $(document).on('mousedown', function(e){
+    startPos = e.clientY; //starting Y of client
+    })
+
+  $(document).on('mouseup', function(e){
+      endPos = e.clientY; //stopping Y of client
+      if ((endPos - startPos) > scrollDelta){
+        console.log("startPos"+ startPos + "endPos" + endPos + "Delta" + (endPos-startPos))
+      }
+  })
+}

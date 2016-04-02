f22c7bf7dc1779094a26069d229ef139c0b73271
Gradecak <gradecam@tcd.ie>
2016-04-02 11:55:54 +0100

    check if the user is at the top of the page before taking any action in mouse down handler

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,11 @@
-$(document).on('click', function(e){
-  console.log(e)
+// distance the user must scroll for it to be considered a swipe
+//  TODO change scroll distance to depend on screen resolution
+var scrollDistance = 40;
+
+$(document).on('mousedown', function(e){
+  // if the user is at the top of the page
+  if($(window).scrollTop() == 0){
+    console.log("Page X:"+ e.pageX + "Page Y" + e.pageY)
+    console.log("Client X:"+ e.clientX + "Client Y" + e.clientY)
+  }
 })

e6707705a52d9c7be8625c3b56045f7c11a4518d
Gradecak <gradecam@tcd.ie>
2016-04-02 13:41:21 +0100

    put refresh gesture in function/ fixed bug where page would refresh when not at top

--- js/content_script.js
+++ js/content_script.js
@@ -1,30 +1,31 @@
+$(document).ready(refreshGesture)
 // distance the user must scroll for it to be considered a swipe
 //  TODO change scroll distance to depend on screen resolution
 var scrollDelta = 100;
 //drag gesture handler
 // if the user is at the top of the page
-if($(window).scrollTop() === 0){
-  var startPos
-  var endPos
-
-  $(document).bind('mousedown touchstart', function(e){
-      if (e.type == "mousedown"){
-        startPos = e.pageY; // starting Y of client
-      }
-      else{
-        startPos = e.originalEvent.targetTouches[0].pageY;
-      }
-    })
-
-  $(document).bind('mouseup touchend', function(e){
-      if (e.type == "mouseup"){
-        endPos = e.pageY; // ending Y of client
-      }
-      else{
-        endPos = e.originalEvent.changedTouches[0].pageY;
-      }
-      if ((endPos - startPos) > scrollDelta){
-        location.reload()
-      }
-  })
+function refreshGesture(){
+var startPos
+var endPos
+$(document).bind('mousedown touchstart', function(e){
+  if (e.type == "mousedown"){
+    startPos = e.screenY; // starting Y of client
+  }
+  else{
+    startPos = e.originalEvent.targetTouches[0].screenY;
+  }
+})
+$(document).bind('mouseup touchend', function(e){
+  if($(window).scrollTop() === 0){
+    if (e.type == "mouseup"){
+      endPos = e.screenY; // ending Y of client
+    }
+    else{
+      endPos = e.originalEvent.changedTouches[0].screenY;
+    }
+    if ((endPos - startPos) > scrollDelta){
+      location.reload()
+    }
+  }
+})
 }

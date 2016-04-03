61321034d9706ec7af46e20ba6e0eb1602a5bc9b
Tomas Barry <tomas.barry@hotmail.com>
2016-04-03 17:50:09 +0100

    Split checks into functions and refined some of the measures

--- js/content_script.js
+++ js/content_script.js
@@ -2,10 +2,8 @@ $(document).ready(main)
 
 
 function main() {
-  var screen_X = $(window).width()
-  var screen_Y = $(window).height()
   refreshGesture()
-  doubleTap(screen_X, screen_Y)
+  doubleTap()
 }
 
 // distance the user must scroll for it to be considered a swipe
@@ -50,46 +48,100 @@ function refreshGesture() {
 }
 
 
-function doubleTap(screen_X, screen_Y) {
-  var eventList = new Array()
-  $(document).bind('click', function(e) {
-    if (eventList.length > 0) {
-      if (window.performance.now() - eventList[0][1] < 1500 && Math.abs(e.screenX - eventList[0][0]) < 30) {
-        if(eventList[0][2] == 0) {
-          chrome.runtime.sendMessage({action: "doubletap-left"}, null);
-        }
-        else if (eventList[0][2] == 1) {
-          chrome.runtime.sendMessage({action: "doubletap-right"}, null);
-        }
-        else if (eventList[0][2] == 2) {
-          console.log("Bottom Left");
-        }
-        else if (eventList[0][2] == 3) {
-          console.log("Bottom Right");
-        }
-        else {
-        	console.log("Double tap for no action");
-        }
-      }
-      eventList = []
-    }
-    else {
-    	var p = -1;
-    	if((e.screenX < (screen_X * .25)) && (e.screenY < (screen_Y *  .40))) {  //Top Left
-    		p = 0;
-    	}
-    	else if((e.screenX > (screen_X*.75)) && (e.screenY < (screen_Y * .40))) { //Top Right
-    		p = 1;
-    	}
-      else if((e.screenX < (screen_X*.25)) && (e.screenY > (screen_Y * .60))) { //Bottom Left
-        p = 2;
-      }
-      else if((e.screenX > (screen_X*.75)) && (e.screenY > (screen_Y * .60))) { //Bottom Right
-        p = 3;
-      }
-    	console.log(p)
-      var eventInfo = [e.screenX, window.performance.now(), p]
-      eventList.push(eventInfo)
-    }
-  })
+function topLeft(clickX, clickY) {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	console.log("Width: " + width);
+	console.log("Height: " + height);
+	if((clickX < (0.25 * width)) && (clickY < (0.40 * height))) {
+		console.log("User clicked in top left");
+		return true;
+	}
+	return false;
+}
+
+
+function bottomLeft(clickX, clickY) {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	console.log("Width: " + width);
+	console.log("Height: " + height);
+	if((clickX < (0.25 * width)) && (clickY > (0.60 * height))) {
+		console.log("User clicked in bottom left");
+		return true;
+	}
+	return false;
+}
+
+
+function topRight(clickX, clickY) {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	console.log("Width: " + width);
+	console.log("Height: " + height);
+	if((clickX > (0.55 * width)) && (clickY < (0.40 * height))) {
+		console.log("User clicked in top right");
+		return true;
+	}
+	return false;
+}
+
+
+function bottomRight(clickX, clickY) {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	console.log("Width: " + width);
+	console.log("Height: " + height);
+	if((clickX > (0.55 * width)) && (clickY > (0.60 * height))) {
+		console.log("User clicked in bottom right");
+		return true;
+	}
+	return false;
+}
+
+
+function doubleTap() {
+  	var eventList = new Array()
+  	$(document).bind('click', function(e) {
+	    if (eventList.length > 0) {
+			if (window.performance.now() - eventList[0][1] < 1500 && Math.abs(e.screenX - eventList[0][0]) < 30) {
+				if(eventList[0][2] == 0) {
+				  	chrome.runtime.sendMessage({action: "doubletap-left"}, null);
+				}
+				else if (eventList[0][2] == 1) {
+				  	chrome.runtime.sendMessage({action: "doubletap-right"}, null);
+				}
+				else if (eventList[0][2] == 2) {
+				  	console.log("Bottom Left");
+				}
+				else if (eventList[0][2] == 3) {
+				  	console.log("Bottom Right");
+				}
+				else {
+					console.log("Double tap for no action");
+				}
+			}
+			eventList = []
+	    }
+	    else {
+	    	var p = -1;
+	    	console.log("Users click event");
+	    	console.log(e);
+	    	if(topLeft(e.screenX, e.screenY)) {
+	    		p = 0;
+	    	}
+	    	else if(topRight(e.screenX, e.screenY)) {
+	    		p = 1;
+	    	}
+	      	else if(bottomLeft(e.screenX, e.screenY)) {
+	        	p = 2;
+	      	}
+	      else if(bottomRight(e.screenX, e.screenY)) {
+	        	p = 3;
+	      }
+	      console.log(p)
+	      var eventInfo = [e.screenX, window.performance.now(), p]
+	      eventList.push(eventInfo)
+	    }
+	})
 }
